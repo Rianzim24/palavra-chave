db4199949a9cdb19da1cf102b628b5920269dd7e
Rianzim24 <santo.rian@escola.pr.gov.br>
2025-08-26 10:30:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 function mostraPalavrasChave() { 
     const texto = document.querySelector("#entrada-de-texto").value;
     const campoResultado = document.querySelector("#resultado-palavrachave");
+    const palavrasChave = processaTexto(texto);
 
-    campoResultado.textContent = texto;
+    campoResultado.textContent = palavrasChave.join(", ")
 }
+    function processaTexto(texto){
+    let palavras = texto.split(/\P{L}+/u);
+    return palavras;
+    }
